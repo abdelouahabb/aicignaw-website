2fefa50ff5f02e697d54588c79642d9d3847d678
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-24 09:47:28 +0800

    Delete runner.sh

--- runner.sh
+++
@@ -1,7 +0,0 @@
-DOCKER_IMG="aicignaw:latest"
-REPO="us-east1-docker.pkg.dev/aicignaw-318216/cloudrunner"
-
-docker build -t ${DOCKER_IMG} .
-docker tag ${DOCKER_IMG} ${REPO}/${DOCKER_IMG}
-echo ${DOCKER_IMG} ${REPO}/${DOCKER_IMG}
-docker push ${REPO}/${DOCKER_IMG}
